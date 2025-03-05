59d91cea4cee27444862b73b3f93eb4d339cfccb
Anand kumar <anand969507@gmail.com>
2025-03-05 18:22:20 +0530

    array program

--- Array.java
+++ Array.java
@@ -119,7 +119,25 @@ public class Array {
     Syntax
     datatype array_name[][]=new data_type[][]
  */
-import java.util.*;
+//---------print matrix and pattern --------
+/*
+    1 2 3
+    4 5 6
+    7 8 9
+  2>:
+    1   3
+      5
+    7   9
+   3>:
+       2   
+     4   6
+       8 
+    4>:
+    1
+      5
+        9
+*/ 
+/*import java.util.*;
 public class Array {
 
     public static void main(String[] args) {
@@ -181,6 +199,135 @@ public class Array {
                 {
                     System.out.print(" ");
                 }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+//========Sum of two array=========
+/*import java.util.*;
+public class Array {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int arr[][]=new int[3][3];
+        int arr1[][]=new int[3][3];
+        int arr2[][]=new int[3][3];
+        int i,j;
+        System.out.println("Enter element in first matrix=");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                arr[i][j]=obj.nextInt();
+            }
+        }
+
+        System.out.println("Enter element in second matrix=");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                arr1[i][j]=obj.nextInt();
+            }
+        }
+        System.out.println("sum of two matrix=");
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                arr2[i][j]=arr[i][j]+arr1[i][j];
+            }
+        }
+
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                System.out.print(arr2[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+//========sum of upper element and sum lower element=========
+/* 
+import java.util.*;
+public class Array {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int arr[][]=new int[3][3];
+        
+        int i,j,l=0,u=0;
+        System.out.println("Enter element in first matrix=");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                arr[i][j]=obj.nextInt();
+            }
+        }
+
+        
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+              if(i<j)
+              {
+                u=u+arr[i][j];
+              }
+              if(i>j)
+              {
+                l=l+arr[i][j];
+              }
+            }
+        }
+        System.out.print("sum of upper matrix elemnet="+u);
+        System.out.println("\nsum of lower matrix elemnet="+l);
+    }
+}*/
+
+
+
+
+//-------Multiplication of matrix-------
+import java.util.*;
+public class Array {
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int arr[][]=new int[3][3];
+        int arr1[][]=new int[3][3];
+        int arr2[][]=new int[3][3];
+        int i,j;
+        System.out.println("Enter element in first matrix=");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                arr[i][j]=obj.nextInt();
+            }
+        }
+
+        System.out.println("Enter element in second matrix=");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                arr1[i][j]=obj.nextInt();
+            }
+        }
+        System.out.println("multiplication of matrix=");
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                arr2[i][j]=0;
+                for(int k=0;k<3;k++){
+                    arr2[i][j]+=arr[i][k]*arr1[k][j];
+                }
+                System.out.print(arr2[i][j]+" ");
             }
             System.out.println();
         }
